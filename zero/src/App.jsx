04404138c5c6c5eb74032a10a50fdafe61ee9437
legendaryPark <pbjkmn00@gmail.com>
2024-04-05 15:41:37 -0700

    Applied modules to components

--- zero/src/App.jsx
+++ zero/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header.jsx";
+import AppHeader from "./components/AppHeader.jsx";
 import Interview from "./components/Interview.jsx";
 import Message from "./components/Message.jsx";
 import Info from "./components/Info.jsx";
@@ -7,7 +7,7 @@ import Info from "./components/Info.jsx";
 function App() {
   return (
     <div id="app-div">
-      <Header />
+      <AppHeader />
       <Info />
       <main id="main-id">
         <Interview id="interview" description="empty" />
--- zero/src/components/Interview.jsx
+++ zero/src/components/Interview.jsx
@@ -1,27 +1,30 @@
-import InterviewBtn from "./InterviewBtn";
-import { useState } from 'react';
-import './Interview.css'
+import Button from "./common/Button";
+import { useState } from "react";
+import "./Interview.css";
+import Section from "./common/Section";
 
 function Interview(props) {
-	const [selectedBtn, setSelectedBtn] = useState('');
+  const [selectedBtn, setSelectedBtn] = useState("");
 
-	function clickHandler(selectedButton) {
-		setSelectedBtn(selectedButton);
-	}
+  function clickHandler(selectedButton) {
+    setSelectedBtn(selectedButton);
+  }
 
-	return (
-		<div id="interview">
-			<h1>Interview Screen</h1>
-			<h1>{props.description}</h1>
-			{selectedBtn}
-			<section id="menubtn">
-				<InterviewBtn onSelect={() => clickHandler('record')}>Record</InterviewBtn>
-				<InterviewBtn onSelect={() => clickHandler('stop')}>Stop</InterviewBtn>
-				<InterviewBtn onSelect={() => clickHandler('screen share')}>Screen share</InterviewBtn>
-				<InterviewBtn onSelect={() => clickHandler('mute')}>Mute</InterviewBtn>
-			</section>
-		</div>
-	);
+  return (
+    <div className="interview">
+      <h1>Interview Screen</h1>
+      <h1>{props.description}</h1>
+      {selectedBtn}
+      <Section className={"menu-btn"}>
+        <Button onSelect={() => clickHandler("record")}>Record</Button>
+        <Button onSelect={() => clickHandler("stop")}>Stop</Button>
+        <Button onSelect={() => clickHandler("screen share")}>
+          Screen share
+        </Button>
+        <Button onSelect={() => clickHandler("mute")}>Mute</Button>
+      </Section>
+    </div>
+  );
 }
 
 export default Interview;
