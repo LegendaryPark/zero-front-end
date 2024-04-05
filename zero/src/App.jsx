321791d19f55dfd0e1531968a1ef23d097f20b29
legendaryPark <pbjkmn00@gmail.com>
2024-04-05 15:50:24 -0700

    Updated components with modules

--- zero/src/App.jsx
+++ zero/src/App.jsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header.jsx'
-import Interview from './components/Interview.jsx'
-import Message from './components/Message.jsx'
-import Info from './components/Info.jsx'
-
+import "./App.css";
+import AppHeader from "./components/AppHeader.jsx";
+import Interview from "./components/Interview.jsx";
+import Message from "./components/Message.jsx";
+import Info from "./components/Info.jsx";
 
 function App() {
-	return (
-		<div id="app-div">
-			<Header />
-			<Info />
-			<main id="mainid">
-				<Interview id="interview" description="empty"/>
-				<Message id="message"/>
-			</main>
-			<footer>
-				<h5>footer</h5>
-			</footer>
-		</div>
-	)
+  return (
+    <div className="app-div">
+      <AppHeader />
+      <Info />
+      <main className="main-id">
+        <Interview className="interview" description="empty" />
+        <Message className="message" />
+      </main>
+      <footer>
+        <h5>footer</h5>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
